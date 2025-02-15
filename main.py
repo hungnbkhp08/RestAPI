1362d5333960e3f066ba474dde378ba98ec8ec15
hungnbkhp08 <hungdepzaihehe@gmail.com>
2025-02-15 21:58:44 +0700

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import jwt
 from passlib.context import CryptContext
 
 app = FastAPI()
-SECRET_KEY = "your_secret_key"
+SECRET_KEY = "hungkien"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
